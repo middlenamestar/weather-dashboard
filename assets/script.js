aa91e41edb2c9c2bc790711625652e2252b525b7
priscilla ma <priscillama621@gmail.com>
2022-02-01 17:31:12 -0500

    adding html, css, js files - added comments to js. modified readme img src path.

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+// when user searches for city:
+// presented with current and future conditions, and the city is added to search history.
+
+// current weather conditions for city:
+// city name, date, weather icon, temperature, humidity, wind speed, uv index.
+
+// uv index:
+// colour indicating whether conditions are favorable, moderate, or severe.
+
+// 5-day forecast:
+// date, weather icon, temperature, humidity, wind speed.
+
+// click on city in search history:
+// again presented with current and future conditions for that city.
+
+
+
+
+
+
+
