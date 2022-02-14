2c0f0fc006e860b7657020dc431014d6d705cbdb
priscilla ma <priscillama621@gmail.com>
2022-02-13 19:24:08 -0500

    rearranged functions, made search history cities clickable so data is accessed again when clicked on, added limit to search, added renderhistory function.

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,4 @@
-// when user searches for city:
-// presented with current and future conditions, and the city is added to search history.
-
-// current weather conditions for city:
-// city name💟, date💟, weather icon💟, temperature💟, humidity💟, wind speed💟, uv index.💟
-
-// uv index:
-// colour indicating whether conditions are favorable, moderate, or severe.
-// (use class)🤎
-
-// 5-day forecast:
-// date, weather icon, temperature, humidity, wind speed.
-
-// click on city in search history:
-// again presented with current and future conditions for that city.
-
-
-
-// current weather:
-// allows you to call by city.
-// name, temperature, humidity, wind speed, icon.
-// missing: uv index.
-
-// 5 day:
-// allows you to call by city.
-// temperature, humidity, wind speed, icon.✅
-
-// one-call:
-// does not allow you to call by city.
-// how to convert city into lat lon because user will type city then that has to go to geo api that converts
-// to lat lon, then geo api fetches data from one-call to get the weather info needed.
-
-// geo: https://api.openweathermap.org/geo/1.0/direct?q=Atlanta&appid=8ee06b00fae45decc9bb95425a69991a💟
-// endpoint & resources: https://api.openweathermap.org/geo/1.0/direct?q=💟
-// user input = ""💟
-// key: &appid=8ee06b00fae45decc9bb95425a69991a💟
-// this would give us geo information.💟
-// grab lat & lon from geo. state, and country, too if liked.💟
-// https://api.openweathermap.org/data/2.5/onecall?lat=33.7489924&lon=-84.3902644&exclude=minutely,hourly,alerts&appid=8ee06b00fae45decc9bb95425a69991a💟
-// https://api.openweathermap.org/data/2.5/onecall?lat= INSERT LAT HERE💟
-// &lon= INSERT LON HERE💟
-// &exclude=minutely,hourly,alerts&appid=8ee06b00fae45decc9bb95425a69991a💟
-// this is one-call.💟
-// now you go into one-call and grab data needed.
-
-// how would you insert lat and lon though?💟
-// create var for lat and lon?💟
-
-// also have to think about search feature.💟
-
-// 2ND SECTION: CURRENT WEATHER.
+// CURRENT WEATHER.
 var date = moment();
 $("#current-date").text(date.format("dddd, MMMM Do, YYYY"));
 var cityName = $("#city-name");
@@ -74,6 +24,7 @@ var dateFive = date.add(1, 'days').format("dddd, MMMM Do");
 $("#date-five").text(dateFive);
 // console.log(dateFive);
 
+// forecast
 var dateOne = $("#date-one");
 var tempOne = $("#temp-one");
 var humidOne = $("#humid-one");
@@ -99,36 +50,30 @@ var tempFive = $("#temp-five");
 var humidFive = $("#humid-five");
 var windFive = $("#wind-five");
 
-
 // FORM
 var searchBtn = $("#search-btn");
 var cityInput = $("#city-input");
 var searchHistory = $("#history");
 
 // array
-var userInput = [];
-
-
-
-
-
+var city = JSON.parse(localStorage.getItem("cities")) || [];
 
 // SET LONDON AS DEFAULT.
-function london(){
+function london() {
     // this fetch is just to get the name to appear on page b/c onecall does not have NAME in data.
     fetch("https://api.openweathermap.org/geo/1.0/direct?q=london&appid=8ee06b00fae45decc9bb95425a69991a")
-        .then(function(response){
-        return response.json();
+        .then(function (response) {
+            return response.json();
         })
-        .then(function(data){
+        .then(function (data) {
             var state = data[0].state;
             var name = data[0].name;
             cityName.text("Currently in: " + name + ", " + state);
             fetch("https://api.openweathermap.org/data/2.5/onecall?lat=51.5073219&lon=-0.1276474&exclude=minutely,hourly,alerts&units=imperial&appid=8ee06b00fae45decc9bb95425a69991a")
-                .then(function(response){
+                .then(function (response) {
                     return response.json();
                 })
-                .then(function(data){
+                .then(function (data) {
                     var currentTemp = data.current.temp;
                     currentTempIcon.text(currentTemp + "˚");
                     var currentIcon = data.current.weather[0].icon;
@@ -141,13 +86,13 @@ function london(){
                     currentWindSpeed.text("Wind Speed: " + currentWind + " mph");
                     var currentUVIndex = data.current.uvi;
                     currentUV.text(currentUVIndex);
-                        if(currentUVIndex >= 0 && currentUVIndex <= 2){
-                            currentUV.addClass("uv-green");
-                        } else if(currentUVIndex >= 3 && currentUVIndex <= 7){
-                            currentUV.addClass("uv-orange");
-                        } else if(currentUVIndex >= 8){
-                            currentUV.addClass("uv-red");
-                        }
+                    if (currentUVIndex >= 0 && currentUVIndex <= 2) {
+                        currentUV.addClass("uv-green");
+                    } else if (currentUVIndex >= 3 && currentUVIndex <= 7) {
+                        currentUV.addClass("uv-orange");
+                    } else if (currentUVIndex >= 8) {
+                        currentUV.addClass("uv-red");
+                    }
                     //5 DAY.
                     // first day.
                     // console.log(data);
@@ -192,7 +137,7 @@ function london(){
                     $("#four-icon").html(iconFourImage);
                     var temperatureFour = data.daily[3].temp.day;
                     tempFour.text("Temperature during day: " + temperatureFour + "˚");
-                    var humidityFour= data.daily[3].humidity;
+                    var humidityFour = data.daily[3].humidity;
                     humidFour.text("Humidity: " + humidityFour + "%");
                     var windSpeedFour = data.daily[3].wind_speed;
                     windFour.text("Wind Speed: " + windSpeedFour + " mph");
@@ -211,42 +156,13 @@ function london(){
         });
 };
 
-
-
-// when user clicks search button: saves city(s) in local storage, appends to search history, calls apis & displays current and forecast info.
-function searchCity(event){
-    event.preventDefault();
-    userInput.push(cityInput.val());
-    localStorage.setItem("cities", JSON.stringify(userInput));
-    // console.log(userInput);
-    var city = JSON.parse(localStorage.getItem("cities"));
-    var lastCity = city.pop();
-    $(`<p>${lastCity}</p>`).appendTo(searchHistory);
-
-    
-// function displayHistory(){
-//     var city = JSON.parse(localStorage.getItem("cities"));
-//     var lastCity = city.pop();
-    // console.log(lastCity);
-
-    // simply appends to search history area.
-    // $(`<p>${lastCity}</p>`).appendTo(searchHistory);
-
-    // gotta make use of the last city variable in order to display current weather, and forecast data.
-// }
-
-// function currentWeather(){
-// }
-
-// var i = searchHistory.length - 1; i >= 0; i--
-
-
+function searchCity(lastCity){
     // GEO
     fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + lastCity + "&appid=8ee06b00fae45decc9bb95425a69991a")
-        .then(function(response){
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data){
+        .then(function (data) {
             var lat = data[0].lat;
             var lon = data[0].lon;
             var state = data[0].state;
@@ -255,10 +171,10 @@ function searchCity(event){
             cityName.text("Currently in: " + name + ", " + state);
             // ONE CALL
             fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=8ee06b00fae45decc9bb95425a69991a")
-                .then(function(response){
+                .then(function (response) {
                     return response.json();
                 })
-                .then(function(data){
+                .then(function (data) {
                     // console.log(data.current); // ok got current weather data. .daily and array [0-7] gives you 8-day forecast.
                     var currentTemp = data.current.temp;
                     currentTempIcon.text(currentTemp + "˚");
@@ -273,13 +189,13 @@ function searchCity(event){
                     currentWindSpeed.text("Wind Speed: " + currentWind + " mph");
                     var currentUVIndex = data.current.uvi;
                     currentUV.text(currentUVIndex);
-                        if(currentUVIndex >= 0 && currentUVIndex <= 2){
-                            currentUV.addClass("uv-green");
-                        } else if(currentUVIndex >= 3 && currentUVIndex <= 7){
-                            currentUV.addClass("uv-orange");
-                        } else if(currentUVIndex >= 8){
-                            currentUV.addClass("uv-red");
-                        }
+                    if (currentUVIndex >= 0 && currentUVIndex <= 2) {
+                        currentUV.addClass("uv-green");
+                    } else if (currentUVIndex >= 3 && currentUVIndex <= 7) {
+                        currentUV.addClass("uv-orange");
+                    } else if (currentUVIndex >= 8) {
+                        currentUV.addClass("uv-red");
+                    }
                     //5 DAY FORECAST
                     // first day.
                     var iconOne = data.daily[0].weather[0].icon;
@@ -324,7 +240,7 @@ function searchCity(event){
                     $("#four-icon").html(iconFourImage);
                     var temperatureFour = data.daily[3].temp.day;
                     tempFour.text("Temperature during day: " + temperatureFour + "˚");
-                    var humidityFour= data.daily[3].humidity;
+                    var humidityFour = data.daily[3].humidity;
                     humidFour.text("Humidity: " + humidityFour + "%");
                     var windSpeedFour = data.daily[3].wind_speed;
                     windFour.text("Wind Speed: " + windSpeedFour + " mph");
@@ -343,59 +259,46 @@ function searchCity(event){
         });
 };
 
+// stays on page on refresh.
+function renderHistory(){
+    searchHistory.text("");
+    for (var i = 0; i < city.length; i++){
+        $(`<p>${city[i]}</p>`).appendTo(searchHistory);
+    };
+};
 
 london();
-searchBtn.on("click", searchCity);
-// $("#city-again").on("click", searchCity);
+renderHistory();
 
+// click on search history cities.
+searchHistory.on("click", function (event){
+    var cityAgain = event.target.innerHTML;
+    // passes cityAgain into searchCity function.
+    searchCity(cityAgain)
+})
 
+// click search button.
+searchBtn.on("click", function (event){
+    event.preventDefault();
+    var cityVal = cityInput.val();
 
-// let searchH = []; //final thing locstor will pull from.
+    // if local storage DOES NOT include what was just searched...
+    if(!city.includes(cityVal)){
+        // push into array
+        city.push(cityVal);
 
-// when user clicks on search, that is when userinput gets stored into locstor, in an array. and with each input, gets stored. push?
-// set the empty array in locstor and keep pushing?
+        // limit searches.
+        if(city.length == 4){
+            city.shift();
+            searchHistory.find(":first-child").remove();
+        };
 
-// searchbutton.on("click", function)
-// function(){
-    // #city-input.push(newArray);
-// }
-// also, if newArray.lenth < 5{
-    // function 
-// } else{
-// }
+        localStorage.setItem("cities", JSON.stringify(city));
+        
+        // appends the city that was just searched for.
+        $(`<p>${cityVal}</p>`).appendTo(searchHistory);
 
-// stringify
-
-
-
-
-
-// have to hard code a location or name in to the LOCATION h3 so that it's not blank when user has not inputted.🤎
-// for temperature and icon, too. pretty much all the information...🤎
-
-
-// data.daily.length = 5;
-// console.log(data.daily);
-// data.daily[0] for the 1st day. data.daily[0].humidity
-// data.daily[0].wind_speed   data.daily[0].temp.day    data.daily[0].weather[0].icon
-// data.daily.length = 5;
-// for(var i = 0; i < data.length; i++){
-    // var dailyHumidity = data.daily[i].humidity;
-    // var dailyWind = data.daily[i].windspeed;
-    // var dailyTemp = data.daily[i].temp.day;
-    // var dailyIcon = data.daily[i].weather[0].icon;
-// }
-
-// unfinished loop. just decided to hard code the forecast.
-// data.daily.length = 5;
-// for(var i = 0; i < data.daily.length; i++){
-//     var forecastDateIcon = $()
-//     var dailyTemp = data.daily[i].temp.day;
-//     var dailyIcon = data.daily[i].weather[0].icon;
-//     var dailyIconImage = new Image();
-//     dailyIconImage.src = "http://openweathermap.org/img/wn/" + dailyIcon + ".png";
-//     var dailyHumidity = data.daily[i].humidity;
-//     var dailyWind = data.daily[i].windspeed;
-//     var forecastContainer = $("#forecast");
-//     $(`${dailyIconImage}`).appendTo(mainBox)
-// }
+        // passes argument into cityName parameter.
+        searchCity(cityVal);
+    };
+});
